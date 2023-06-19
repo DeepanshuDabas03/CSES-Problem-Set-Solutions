dbbe2d3ea56dad708a55ec092c481581adef57ec
Deepanshu Dabas <104428415+DeepanshuDabas03@users.noreply.github.com>
2023-06-19 23:31:44 +0530

    Create Dice Combinations Bottom.cpp

---
+++ Dice Combinations Bottom.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+typedef long long ll;
+const ll mod = 1e9 + 7;
+int dp[1000001];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	ll n;
+	cin >> n;
+	for (int j = 1; j <= n; j++)
+	{
+		for (int i = 1; i <= 6; i++)
+		{
+			if (j - i >= 0)
+			{
+				if (j - i == 0)
+					dp[j] = (dp[j] + 1) % mod;
+				else
+					dp[j] = (dp[j] + dp[j - i]) % mod;
+			}
+		}
+	}
+	cout << dp[n] << endl;
+	return 0;
+}
